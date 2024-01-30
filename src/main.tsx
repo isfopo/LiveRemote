7e4f10957250394f9b11bea2acbd2c6301923957
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-01-29 23:09:04 -0500

    fixes linting issues

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React = require("react");
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
@@ -7,7 +7,7 @@ import "@tamagui/core/reset.css";
 import { TamaguiProvider } from "tamagui";
 import config from "../tamagui.config.ts";
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <TamaguiProvider config={config}>
       <App />
