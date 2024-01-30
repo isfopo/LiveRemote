dfc684d6249bc280cf192b820c93f7cb465529b5
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-01-30 00:24:16 -0500

    adds middleware for socket

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducers";
+import { socketMiddleware } from "./middleware/socketMiddleware";
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().prepend(socketMiddleware),
 });
 
+export type AppDispatch = typeof store.dispatch;
 export default store;
