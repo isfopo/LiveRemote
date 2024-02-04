46d79e1145446e0e7d72fdfc174b97393a01c3ca
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-02-04 11:13:55 -0500

    echos message back to client

--- script/Server.py
+++ script/Server.py
@@ -1,10 +1,11 @@
+import os
 import socket
 import threading
 import json
-from _Framework import ControlSurface
 import base64
 import hashlib
 import struct
+from _Framework import ControlSurface
 
 PORT = 8000
 
@@ -21,10 +22,10 @@ class Server(threading.Thread):
         sock.listen(1)
 
         while True:
-            conn, addr = sock.accept()
-            print("Connected by", addr)
+            self.conn, self.addr = sock.accept()
+            print("Connected by", self.addr)
 
-            data = conn.recv(1024)
+            data = self.conn.recv(1024)
             headers = self.parse_headers(data)
 
             # Perform WebSocket handshake
@@ -32,29 +33,38 @@ class Server(threading.Thread):
             resp_key = self.calculate_response_key(key)
             handshake_response = self.create_handshake_response(resp_key)
 
-            conn.sendall(handshake_response.encode())
+            self.conn.sendall(handshake_response.encode())
 
             # Handle WebSocket frames
             while True:
-                frame = conn.recv(2)
+                frame = self.conn.recv(2)
                 if not frame:
                     break
 
                 first_byte, second_byte = struct.unpack("!BB", frame)
+
                 fin = (first_byte & 0b10000000) >> 7
                 opcode = first_byte & 0b00001111
+                rsv1 = (first_byte & 0b01000000) >> 6
+                rsv2 = (first_byte & 0b00100000) >> 5
+                rsv3 = (first_byte & 0b00010000) >> 4
                 mask = (second_byte & 0b10000000) >> 7
                 payload_length = second_byte & 0b01111111
 
+                if rsv2 or rsv3:
+                    raise ValueError(
+                        "Invalid WebSocket frame: RSV2 and RSV3 must be clear"
+                    )
+
                 if mask:
-                    masking_key = conn.recv(4)
+                    masking_key = self.conn.recv(4)
 
                 if payload_length == 126:
-                    payload_length = struct.unpack("!H", conn.recv(2))[0]
+                    payload_length = struct.unpack("!H", self.conn.recv(2))[0]
                 elif payload_length == 127:
-                    payload_length = struct.unpack("!Q", conn.recv(8))[0]
+                    payload_length = struct.unpack("!Q", self.conn.recv(8))[0]
 
-                payload_data = conn.recv(payload_length)
+                payload_data = self.conn.recv(payload_length)
 
                 if mask:
                     unmasked_data = bytearray()
@@ -66,6 +76,9 @@ class Server(threading.Thread):
                 payload = self.parse_payload_to_object(payload_data)
 
                 self.control_surface.log_message(payload["hi"])
+
+                # Send a response back to the client
+                self.send(payload)
 
     def parse_headers(self, data):
         headers = {}
@@ -95,3 +108,29 @@ class Server(threading.Thread):
             return json.loads(payload_data.decode())
         except json.JSONDecodeError:
             return None
+
+    def send(self, payload: dict) -> None:
+        # Convert the payload object to a JSON string
+        payload_json = json.dumps(payload)
+
+        # Send the header and payload through the socket connection
+        self.conn.sendall(self.create_websocket_header(payload_json.encode("utf-8")))
+
+    def create_websocket_header(self, payload):
+        header = bytearray()
+
+        # Set the FIN bit to 1 and opcode to 1 for a text message
+        header.append(0b10000001)
+
+        # Set the mask bit to 0 (masking is not required for server-to-client messages)
+        payload_length = len(payload)
+        if payload_length < 126:
+            header.append(payload_length)
+        elif payload_length < 65536:
+            header.append(126)
+            header.extend(struct.pack("!H", payload_length))
+        else:
+            header.append(127)
+            header.extend(struct.pack("!Q", payload_length))
+
+        return header + payload
