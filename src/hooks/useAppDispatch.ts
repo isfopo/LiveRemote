ba1768d1d8ea9de17053464587c33cf9687a9520
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-02-07 00:48:26 -0500

    removes redux hooks

--- src/hooks/useAppDispatch.ts
+++
@@ -1,6 +0,0 @@
-import { useDispatch } from "react-redux";
-import { RootState } from "../store/store";
-import { Action, ThunkDispatch } from "@reduxjs/toolkit";
-
-export const useAppDispatch = () =>
-  useDispatch<ThunkDispatch<RootState, null, Action>>();
