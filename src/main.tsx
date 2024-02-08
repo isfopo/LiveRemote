356de94246d6714b0d5dc495e33476fd7ea93ffa
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-02-07 21:30:28 -0500

    creates router

--- src/main.tsx
+++ src/main.tsx
@@ -9,13 +9,15 @@ import config from "../tamagui.config.ts";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import { SocketProvider } from "./context/socket/SocketProvider.tsx";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router.tsx";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
     <React.StrictMode>
       <SocketProvider>
         <TamaguiProvider config={config}>
-          <App />
+          <RouterProvider router={router} />
         </TamaguiProvider>
       </SocketProvider>
     </React.StrictMode>
---
+++ src/router.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <p>hi</p>,
+  },
+  {
+    path: "/connect",
+    element: <p>connect</p>,
+  },
+]);
