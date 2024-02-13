40a3946f5202450adb292a85e43356df55d97a76
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-02-12 22:02:34 -0500

    closes dialog after checking code

--- src/views/Remote/index.tsx
+++ src/views/Remote/index.tsx
@@ -8,10 +8,18 @@ import { useSocket } from "../../hooks/useSocket";
 export const Remote = () => {
   const { dispatch: dialogDispatch } = useDialogContext();
 
-  const { find, connect, candidates, host, showCode, disconnect, checkCode } =
-    useSocket({
-      auto: true,
-    });
+  const {
+    candidates,
+    host,
+    code,
+    find,
+    connect,
+    showCode,
+    disconnect,
+    checkCode,
+  } = useSocket({
+    auto: true,
+  });
 
   useEffect(() => {
     if (candidates && candidates.length > 0) {
@@ -33,6 +41,15 @@ export const Remote = () => {
       });
     }
   }, [candidates, host, showCode, connect]);
+
+  useEffect(() => {
+    if (host && code) {
+      dialogDispatch({
+        type: "close",
+        payload: null,
+      });
+    }
+  }, [host, code]);
 
   if (candidates.length === 0 && !host) {
     return (
