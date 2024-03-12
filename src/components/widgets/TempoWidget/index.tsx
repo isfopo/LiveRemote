67c14322d1864385128ba88db549f533d5b715c0
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-03-11 23:36:06 -0400

    refactors with use gesture

--- src/components/widgets/TempoWidget/index.tsx
+++ src/components/widgets/TempoWidget/index.tsx
@@ -1,9 +1,14 @@
-import React, { useCallback, useEffect, useState } from "react";
+import { useDrag } from "@use-gesture/react";
+import React, { useEffect, useState } from "react";
 import { useLiveContext } from "../../../context/live/useLiveContext";
 import { Method, OutgoingMessage } from "../../../types/socket";
 import { Spinner } from "../../loaders/Spinner";
-
 import { Widget } from "../Widget";
+
+export interface Location {
+  x: number;
+  y: number;
+}
 
 export interface TempoWidgetProps {
   send: (message: OutgoingMessage) => void;
@@ -16,8 +21,7 @@ export const TempoWidget = ({ send }: TempoWidgetProps) => {
     },
   } = useLiveContext();
 
-  const [isClicked, setIsClicked] = useState<boolean>(false);
-  const mouseLocation = React.useRef<MouseEvent | TouchEvent>();
+  const mouseLocation = React.useRef<Location | undefined>();
   const [_tempo, setTempo] = useState<number | undefined>(tempo);
 
   // updates tempo when live context changes
@@ -25,97 +29,47 @@ export const TempoWidget = ({ send }: TempoWidgetProps) => {
     setTempo(tempo);
   }, [tempo]);
 
-  const handleMouseDown = useCallback(
-    (
-      e: React.MouseEvent<HTMLDivElement> | React.TouchEvent<HTMLDivElement>
-    ) => {
-      setIsClicked(true);
-      mouseLocation.current = e.nativeEvent;
-    },
-    []
-  );
+  const bind = useDrag(({ movement: [x, y], last }) => {
+    if (mouseLocation.current === undefined) {
+      mouseLocation.current = {
+        x,
+        y,
+      };
+    } else {
+      const deltaX = Math.abs(x - mouseLocation.current?.x);
+      const deltaY = Math.abs(y - mouseLocation.current?.y);
 
-  const handleMouseMove = useCallback(
-    (
-      e: React.MouseEvent<HTMLDivElement> | React.TouchEvent<HTMLDivElement>
-    ) => {
-      e.preventDefault();
-
-      let deltaX: number | null = null;
-      let deltaY: number | null = null;
-      let shouldIncrease: boolean | null = null;
-
-      if (isClicked && mouseLocation.current && tempo) {
-        if (
-          e.nativeEvent instanceof MouseEvent &&
-          mouseLocation.current instanceof MouseEvent
-        ) {
-          deltaX = Math.abs(
-            e.nativeEvent.screenX - mouseLocation.current.screenX
-          );
-          deltaY = Math.abs(
-            e.nativeEvent.screenY - mouseLocation.current.screenY
-          );
-          shouldIncrease =
-            !!mouseLocation.current?.screenY &&
-            e.nativeEvent.screenY >= mouseLocation.current?.screenY;
-        } else if (
-          e.nativeEvent instanceof TouchEvent &&
-          mouseLocation.current instanceof TouchEvent
-        ) {
-          deltaX = Math.abs(
-            e.nativeEvent.touches[0].screenX -
-              mouseLocation.current.touches[0].screenX
-          );
-          deltaY = Math.abs(
-            e.nativeEvent.touches[0].screenY -
-              mouseLocation.current.touches[0].screenY
-          );
-          shouldIncrease =
-            !!mouseLocation.current?.touches[0].screenY &&
-            e.nativeEvent.touches[0].screenY >=
-              mouseLocation.current?.touches[0].screenY;
-        }
-
-        if (!!deltaX && !!deltaY && deltaX < deltaY) {
-          if (shouldIncrease) {
-            setTempo((t) => t && t - 1);
-          } else {
-            setTempo((t) => t && t + 1);
-          }
+      if (deltaX < deltaY) {
+        if (!!mouseLocation.current?.x && y >= mouseLocation.current?.y) {
+          setTempo((t) => t && t - 1);
+        } else {
+          setTempo((t) => t && t + 1);
         }
       }
 
-      mouseLocation.current = e.nativeEvent;
-    },
-    [tempo, isClicked]
-  );
+      mouseLocation.current = {
+        x,
+        y,
+      };
+    }
 
-  const handleMouseUp = useCallback(() => {
-    setIsClicked(false);
-    send({
-      method: Method.SET,
-      address: "song",
-      prop: "tempo",
-      value: _tempo,
-      type: "float",
-    });
-  }, [_tempo, send]);
+    if (last) {
+      send({
+        method: Method.SET,
+        address: "song",
+        prop: "tempo",
+        value: _tempo,
+        type: "float",
+      });
+    }
+  });
 
   if (!tempo) {
     return <Spinner />;
   }
 
   return (
-    <Widget
-      onMouseDown={handleMouseDown}
-      onMouseMove={handleMouseMove}
-      onMouseUp={handleMouseUp}
-      onMouseLeave={handleMouseUp}
-      onTouchStart={handleMouseDown}
-      onTouchMove={handleMouseMove}
-      onTouchEnd={handleMouseUp}
-    >
+    <Widget {...bind()}>
       <h3>{_tempo?.toFixed(2)}</h3>
     </Widget>
   );
